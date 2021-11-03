b0e65611d8adcabade874c4afb95f136a9066f7e
lebrancconvas <lebrancconvas@gmail.com>
2021-11-03 13:13:50 +0700

    ✨ Add User Route.

--- web/webserver.go
+++ web/webserver.go
@@ -5,13 +5,28 @@ import (
 )
 
 func main() {
+	userNum := map[string]int {
+		"Tarima": 99,
+		"Buga": 199,
+	}
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "NARAKA: Bladepoint"); 
 	})
 
-	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "GITHUB: "); 
-	})
+	// http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
+	// 	fmt.Fprintf(w, "GITHUB: "); 
+	// })
+
+	http.HandleFunc("/about", about);
+	http.HandleFunc("/user/", func(w http.ResponseWriter, r *http.Request) {
+		name := r.URL.Path[len("/user/"):] 
+		point := userNum[name];  
+		fmt.Fprintf(w, "%s: %d", name, point); 
+	});
 
 	http.ListenAndServe(":8080", nil); 
+}
+
+func about(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "GITHUB:"); 
 }
