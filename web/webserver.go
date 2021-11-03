5f023f1696490f38086f2bd8cfee56151e87236f
lebrancconvas <lebrancconvas@gmail.com>
2021-11-03 13:31:01 +0700

    ✅ Add Data.

--- web/webserver.go
+++ web/webserver.go
@@ -8,6 +8,9 @@ func main() {
 	userNum := map[string]int {
 		"Tarima": 99,
 		"Buga": 199,
+		"Akira": 299,
+		"Henry": 302,
+		"Mega": 1999, 
 	}
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "NARAKA: Bladepoint"); 
@@ -19,10 +22,12 @@ func main() {
 
 	http.HandleFunc("/about", about);
 	http.HandleFunc("/user/", func(w http.ResponseWriter, r *http.Request) {
-		name := r.URL.Path[len("/user/"):] 
+		name := r.URL.Path[len("/user/"):];
 		point := userNum[name];  
 		fmt.Fprintf(w, "%s: %d", name, point); 
-	});
+	}); 
+
+	// http.HandleFunc("/user/", user); 
 
 	http.ListenAndServe(":8080", nil); 
 }
@@ -30,3 +35,9 @@ func main() {
 func about(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "GITHUB:"); 
 }
+
+// func user(w http.ResponseWriter, r *http.Request) {
+// 	name := r.URL.path[len("/user/"):];
+// 	point := userNum[name];
+// 	fmt.Fprintf(w, "%s: %d",name, point); 
+// }
